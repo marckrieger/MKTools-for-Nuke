7328c06570f4af7bd71c20da8b4b8e5900717c98
Marc Krieger <mkriegerolle@protonmail.com>
2023-05-17 15:29:23 +0200

    Update changeproperty.py

--- mktools/changeproperty.py
+++ mktools/changeproperty.py
@@ -43,39 +43,3 @@ def create_panel():
 menubar=nuke.menu("Nuke")
 menu_mk = menubar.addMenu("MK Tools")
 menu_mk.addCommand("Batch change property", create_panel)
-
-
-
-
-
-
-# #PANEL creation
-# panel = nuke.Panel('Batch Change Property')
-# # panel.setWidth(700)
-# panel.Multiline_Eval_String_Knob('Explanation of the tool','Hover over the desired property to view its name. Insert a string, float or boolean depending on the property.')
-# panel.addSingleLineInput('Property', '')
-# panel.addSingleLineInput('Value', '')
-
-# result = panel.show()
-
-# if result:
-# #RETRIEVE VALUES
-#     property = panel.value('Property')
-#     value = panel.value('Value')
-#     def valueConversion(valueToConvert):
-#         try:
-#             if valueToConvert.lower() == "true":
-#                 return True
-#             elif valueToConvert.lower() == "false":
-#                 return False
-#             else:
-#                 converted_value = float(valueToConvert)
-#                 return converted_value
-#         except:
-#             return str(valueToConvert)
-
-#     value = valueConversion(value)
-
-#     selectedNodes = nuke.selectedNodes()
-#     for node in selectedNodes:
-#         node.knob(str(property)).setValue(value)
